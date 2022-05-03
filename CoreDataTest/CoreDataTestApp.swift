0827aa1e91852398db3ba3e81413b49df4ca6c0e
cjdvnd0206 <cjdvndaudnjf@naver.com>
2022-05-03 10:15:49 +0900

    Initial Commit

---
+++ CoreDataTest/CoreDataTestApp.swift
@@ -0,0 +1,20 @@
+//
+//  CoreDataTestApp.swift
+//  CoreDataTest
+//
+//  Created by user on 2022/05/03.
+//
+
+import SwiftUI
+
+@main
+struct CoreDataTestApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
